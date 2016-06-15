fb5e45b290ed8124760251b1cfec0470a055b599
Jaroslav Siroic <jaroslav.siroic@gmail.com>
2016-06-15 16:28:50 +0300

    knn stuff

--- src/Knn.java
+++ src/Knn.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,22 +7,27 @@ public class Knn {
 
     public int k;
     public Map<Model,Double> distances = new HashMap<>();
+    private Model uModel;
+    private ArrayList<Model> set;
 
-    private Model model;
+    public Knn(ArrayList<Model> set, Model uModel, int k) {
+        this.set = set;
+        this.uModel = uModel;
+        this.k = k;
+    }
+    public int init(){
 
-    public double distance(Model uModel){
+        return 0;
+    }
+    private double distance(Model model){
         double distance=0;
         //double sum = 0;
-        for(int i=0; i < uModel.vector.length; i++){
-            distance+=Math.pow(uModel.vector[i]-model.vector[i],2);
+        for(int i=0; i < model.vector.length; i++){
+            distance+=Math.pow(model.vector[i]-uModel.vector[i],2);
         }
         distance=Math.sqrt(distance);
         return distance;
     }
 
-    private void putDistanceToHashMap(Model uModel){
-        double distance=distance(uModel);
-        distances.put(uModel,distance);
-    }
 
 }
