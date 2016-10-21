88b2aea8c8a351dd95185b057361b0f676b67090
jaroslavsiroic <jaroslav.siroic@gmail.com>
2016-10-21 14:57:51 +0300

    methog model.clone() added

---
+++ src/Statistics.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
+public class Statistics {
+
+    //input: k, set;
+    //output: percentage;
+
+    public Knn knn = new Knn();
+
+    public double percent ( double checktimes, double matchtimes){
+        return ((matchtimes / checktimes)) * 100;
+    }
+
+    public double calcPercentage(int k, ArrayList<Model> set) {
+        Model model;
+        int rightGuesses = 0;
+        int guess;
+
+        for (int i = 0; i < set.size(); i++) {
+            model = set.get(i);
+            set.remove(model);
+            guess = knn.init(set,model,k);
+            if (guess == model.iClass){
+                rightGuesses++;
+            }
+
+            set.add(model);
+        }
+        return percent(set.size(), rightGuesses);
+    }
+}
